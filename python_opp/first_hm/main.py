a4b410ece5526d659c3feb5fc4e314a004788a0f
Yan <jan.kozhyn@gmail.com>
2022-11-23 22:19:11 +0200

    made changes in method "total_price"

--- python_opp/first_hm/main.py
+++ python_opp/first_hm/main.py
@@ -2,19 +2,15 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
-
     def price_of_product(self):
         return f"Price: {self.price} of product: {self.name}"
-
 
 class ShoppingCart:
     def __init__(self, product: Product, quantity):
         self.products = product
         self.quantity = quantity
-
     def total_price(self):
-        return self.products.price * self.quantity
-
+        return f"Your total price: {self.products.price * self.quantity}"
 
 bear = Product("Bear", 5.5)
 print(bear.price_of_product())
